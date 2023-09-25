c600a1f8d4dcfc56119503ec91e22b4d8510fd0c
Moopen <n.moopen@uu.nl>
2023-09-25 19:04:34 +0200

    got things working for multiple files

--- scripts/qualtrics-download.R
+++ scripts/qualtrics-download.R
@@ -1,9 +1,21 @@
 library(here)
 library(qualtRics)
 library(readr)
+library(purrr)
 
-qualtrics_survey <- fetch_survey(surveyID = "SV_0vMjmokYyu4nt6m",
-                                force_request = TRUE,
-                                verbose = TRUE)
+#qualtrics_survey <- fetch_survey(surveyID = "SV_0vMjmokYyu4nt6m",
+#                                force_request = TRUE,
+#                                verbose = TRUE)
 
-write_csv2(qualtrics_survey, file = here(paste0("data/", "ouders_", Sys.Date(), ".csv")))
+# write_csv2(qualtrics_survey, file = here(paste0("data/", "ouders_", Sys.Date(), ".csv")))
+
+#--------
+  
+survey_ids <- read_csv("config/survey-ids.csv") 
+              
+surveys <- lapply(survey_ids$survey_id, function(x) fetch_survey(x, force_request = TRUE, verbose = TRUE)) %>% 
+  
+  setNames(., survey_ids$survey_abbreviation)
+
+lapply(1:length(surveys), function(x) write_csv(surveys[[x]], 
+                                                file = here(paste0("data/", Sys.Date(), "_", names(surveys[x]), ".csv"))))
--- scripts/yoda-upload.py
+++ scripts/yoda-upload.py
@@ -13,7 +13,27 @@ with open(os.path.expanduser("C:/Users/Moope001/.irods/irods_environment.json"),
     ienv = json.load(f)
 session = iRODSSession(**ienv, password=passwd)
 
+irods_base_dir = '/nluu10p/home/research-hear-hear-child-participation/data/'
+
+data_objects = [os.path.abspath(os.path.join("data", file)).replace("\\", "/") for file in os.listdir("data")] # works!
+
+for object in data_objects:
+        # Get the local file name from the absolute path
+        local_file = os.path.basename(object)
+        
+        # Construct the destination path in iRODS
+        irods_dest_path = os.path.join(irods_base_dir, local_file)
+
+        # Upload the file to iRODS
+        session.data_objects.put(object, irods_dest_path)
+        
+# -----------
+
 # coll = session.collections.get("/"+ session.zone + "/home/")
 # print(coll.subcollections)
 
-session.data_objects.put("C:/Users/Moope001/OneDrive - Universiteit Utrecht/Documents/programming/hear-hear/data/ouders_2023-09-15.csv", "/nluu10p/home/research-hear-hear-child-participation/data/ouders_2023-09-15.csv")
+# files = os.listdir("data") # step 1
+# data_objects = [os.path.abspath(os.path.join("data", file)) for file in files] # step 2
+# data_objects = [os.path.abspath(os.path.join("data", file)) for file in os.listdir("data")] # works! but with windows paths
+ 
+# session.data_objects.put("C:/Users/Moope001/OneDrive - Universiteit Utrecht/Documents/programming/hear-hear/data/ouders_2023-09-25.csv", "/nluu10p/home/research-hear-hear-child-participation/data/ouders_2023-09-25.csv")
